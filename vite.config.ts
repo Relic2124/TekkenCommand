ffe83f9400f75cef8e05b6f0ed1f44a52f31ef24
Relic2124 <gmlakddls1@gmail.com>
2026-02-07 19:18:23 +0900

    commit 2

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [react()],
     root: "./src",
-    base: "/",
+    base: "/TekkenCommand/",
     build: {
-        outDir: "./dist"
+        outDir: "../dist"
     },
     server: {
         port: 3000
